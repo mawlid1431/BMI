d877f66675bc7fb09757321f088a71fe341a39b9
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 14:47:14 +0300

    BMI challenge is done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,33 @@
 // Good Luck! You got this 💪🏾
+
 // Write your code here.
+//
+
+
+
+
+
+function calculateBMI(weight, height) {
+  const bmi = weight / (height * height);
+  return bmi;
+}
+
+// Calculate Ali's BMI
+const aliWeight = 75; // Ali's weight in kilograms
+const aliHeight = 1.75; // Ali's height in meters
+const aliBMI = calculateBMI(aliWeight, aliHeight);
+
+// Calculate Mohamed's BMI
+const mohamedWeight = 80; // Mohamed's weight in kilograms
+const mohamedHeight = 1.80; // Mohamed's height in meters
+const mohamedBMI = calculateBMI(mohamedWeight, mohamedHeight);
+
+// Compare BMIs
+if (aliBMI > mohamedBMI) {
+  console.log("Ali's BMI is higher than Mohamed's.");
+} else if (aliBMI < mohamedBMI) {
+  console.log("Mohamed's BMI is higher than Ali's.");
+} else {
+  console.log("Ali and Mohamed have the same BMI.");
+}
+
